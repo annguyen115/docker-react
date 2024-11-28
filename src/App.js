a8cbf13460a3092e70098861de0bf295887b1ddd
annguyen115 <andqnguyen.work@gmail.com>
2024-11-28 16:39:24 +0700

    Update title text in src/App.js header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world from Amazon Website Services</h1>
+        <h1>This website host in Amazon Website Services</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
